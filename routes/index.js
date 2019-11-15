290231957de6730cddbadfccf177cb15bdb90cb3
hchsiao <hchsiao@vlsilab.ee.ncku.edu.tw>
2019-11-16 00:00:05 +0800

    use promise instead of callback

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/runoob";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({"index": 5});
+  var ipv4 = req.ip.split(":")[3];
+  MongoClient.connect(url, { useUnifiedTopology: true })
+    .then(function (db) {
+      var dbo = db.db("test");
+      var queries = [
+        dbo.collection("persons").find({}).toArray(),
+        dbo.collection("inventory").find({}).toArray(),
+        dbo.collection("orders").find({}).toArray()
+      ]
+      Promise.all(queries)
+        .then(function(ress) {
+          res.json({"ipv4": ipv4, "db": ress});
+          db.close();
+        })
+        .catch(function (err) {
+          console.log("Error when querying");
+          db.close();
+        });
+    })
+    .catch(function (err) {
+      console.log("Error when connecting to DB");
+      db.close();
+    });
 });
 
 module.exports = router;
